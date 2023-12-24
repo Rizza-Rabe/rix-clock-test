c21471024553aae9cc929ad41e8d2214f888ef1f
Rizza-Rabe <rizzabhb24024@gmail.com>
2023-12-24 21:44:30 +0800

    Last

--- main.dart.js
+++ main.dart.js
@@ -104357,7 +104357,7 @@ $S:0}
 A.aqs.prototype={
 EY(){var s=0,r=A.m(t.S),q,p=2,o,n,m,l,k,j,i,h,g,f,e
 var $async$EY=A.i(function(a,b){if(a===1){o=b
-s=p}while(true)switch(s){case 0:f=A.eF("https://check-host.net/check-ping?host=www.google.com&max_nodes=3",0,null)
+s=p}while(true)switch(s){case 0:f=A.eF("https://check-host.net/check-http?host=https://1.1.1.1&max_nodes=3",0,null)
 p=4
 n=new A.bc(Date.now(),!1)
 h=t.N
